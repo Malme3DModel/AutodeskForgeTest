9c828dc1d983d1a3fc99a4a0585d6c8e557525ce
sasaco <sasaicco@gmail.com>
2024-01-06 16:42:58 +0900

    token追加

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-const APS_ACCESS_TOKEN = ''; // Specify your access token
-const APS_MODEL_URN = ''; // Specify your model URN
+// Specify your access token
+const APS_ACCESS_TOKEN = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjY0RE9XMnJoOE9tbjNpdk1NU0xlNGQ2VHEwUV9SUzI1NiIsInBpLmF0bSI6Ijd6M2gifQ.eyJzY29wZSI6WyJidWNrZXQ6Y3JlYXRlIiwiZGF0YTpyZWFkIiwiZGF0YTp3cml0ZSJdLCJjbGllbnRfaWQiOiJsU3pZNmFWNENhaDk1RmJWN2M2R1dnSWhoRzhvTjhsaiIsImF1ZCI6Imh0dHBzOi8vYXV0b2Rlc2suY29tL2F1ZC9hand0ZXhwNjAiLCJqdGkiOiI3VFdyNlpOeTV2a1QzaHJWOHU0VEVuNWk0QzdMVFNoTTNhbGl4aTlxMm14eFZySjJueFFCNDQ5cUdZc1F6NlB3IiwiZXhwIjoxNzA0NTI4NTM3fQ.cu7hRPdU8B8i-joi3pbaKzTUtQCrVN76_yM4BswyD2Pzocwx4tDfSjLEro8alPsYJuuFC0isXzD-bw-hiB99fjoI36GLvVg7QjK93y0tOEBomOXS5XULeClSoRMb4Peb5ktUNc-cXZpX6vnGWGj8O4F2T1S8mxEgySGgDGN3sA1OHOknIhzaVmIVEw4jQxSyBOQh14s7AxpBxU4zHwP-W-614PKj0FHGnqPIEe198F3a0JAdb_wHJux7cm2xDSFsLJ91igITMIkNgL-a3MrM2fca8u6BAuvwtSwS3_YOSVjleUufPb50egGyrLbqiXj-yvGxhNvFI5A9h7ZrTRjU0Q'; 
+// Specify your model URN
+const APS_MODEL_URN = 'https://developer.api.autodesk.com/oss/v2/buckets/my_server_side_app_bucket/objects/Clinic_Electrical.ifc'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 if (!APS_ACCESS_TOKEN || !APS_MODEL_URN) {
