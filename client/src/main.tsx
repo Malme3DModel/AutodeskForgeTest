9b2df0a90f873de94174dc011d7a72a1dc13cea9
sasaco <sasaicco@gmail.com>
2024-02-23 10:31:44 +0900

    https://aps.autodesk.com/blog/building-simple-react-wrapper-viewer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './components/App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const APS_ACCESS_TOKEN = ''; // Specify your access token
+const APS_MODEL_URN = ''; // Specify your model URN
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+if (!APS_ACCESS_TOKEN || !APS_MODEL_URN) {
+    root.render(
+      <div>Please specify <code>APS_ACCESS_TOKEN</code> and <code>APS_MODEL_URN</code> in the source code.</div>
+    );
+} else {
+    root.render(
+      <React.StrictMode>
+        <App token={APS_ACCESS_TOKEN} urn={APS_MODEL_URN} />
+      </React.StrictMode>,
+    );
+}
+
