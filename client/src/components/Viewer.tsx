89f5124c7db5d7f1c817f3eb534c890c35b266a2
sasaco <sasaicco@gmail.com>
2024-02-25 11:51:00 +0900

    不要なもの削除

--- client/src/components/Viewer.tsx
+++ client/src/components/Viewer.tsx
@@ -1,7 +1,6 @@
 /// import * as Autodesk from "@types/forge-viewer";
 
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 const { Autodesk } = window;
 
